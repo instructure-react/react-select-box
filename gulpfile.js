d60173ea800facb3fac17d8819846e8a8ecfdc26
Aaron Shafovaloff <aaronshaf@gmail.com>
2014-09-04 14:30:04 -0600

    Start using ESLint

--- gulpfile.js
+++ gulpfile.js
@@ -1,7 +1,10 @@
 var gulp = require('gulp')
 var browserify = require('browserify')
+var eslint = require('gulp-eslint')
+var eslintConfig = require('./eslint.json')
 var source = require('vinyl-source-stream')
 var connect = require('gulp-connect')
+var cache = require('gulp-cached')
 
 gulp.task('browserify', function() {
   return browserify({
@@ -25,9 +28,16 @@ gulp.task('server', function() {
   })
 })
 
+gulp.task('lint', function () {
+  return gulp.src(['lib/**/*.js'])
+    .pipe(cache('linting'))
+    .pipe(eslint(eslintConfig))
+    .pipe(eslint.format())
+})
+
 gulp.task('default', ['browserify'])
 
-gulp.task('serve', ['static', 'browserify', 'server'], function() {
+gulp.task('serve', ['static', 'browserify', 'lint', 'server'], function() {
   gulp.watch(['example/**/*'], ['static', 'browserify'])
   gulp.watch(['lib/**/*'], ['browserify'])
 })
